4fe5b765ec01a72def9bb7bf4ce3e57747535345
amynang <amynangelos@windowslive.com>
2024-09-07 15:58:00 +0200

    ...

--- script.R
+++ script.R
@@ -420,44 +420,146 @@ ggsave("figs/fig4.png", fig4, units = "mm",
 
 
 
+######################### Three Betas under a trenchcoat #######################
+
+m.all.r_hhm2_h.ph <- brm(bf(roots ~ (rel.ht  + leaf_habit + myc_group)^2 + 
+                              (1 + rel.ht|gr(phylo, cov = A)) + (1 + rel.ht|SppName) + (1|Study)), 
+                         data = within.sp, 
+                         data2 = list(A = A),
+                         family = Beta(), 
+                         chains = 4, 
+                         iter = 9000, 
+                         warmup = 3000, 
+                         cores = 4, 
+                         control = list(adapt_delta = 0.99,
+                                        max_treedepth = 12),
+                         backend = "cmdstanr",
+                         file = "fits/m.all.r_hhm2_h.ph",
+                         seed = 123)
+
+m.all.s_hhm2_h.ph <- brm(bf(stems ~ (rel.ht  + leaf_habit + myc_group)^2 + 
+                              (1 + rel.ht|gr(phylo, cov = A)) + (1 + rel.ht|SppName) + (1|Study)), 
+                         data = within.sp, 
+                         data2 = list(A = A),
+                         family = Beta(), 
+                         chains = 4, 
+                         iter = 9000, 
+                         warmup = 3000, 
+                         cores = 4, 
+                         control = list(adapt_delta = 0.99,
+                                        max_treedepth = 12),
+                         backend = "cmdstanr",
+                         file = "fits/m.all.s_hhm2_h.ph",
+                         seed = 123)
+
+m.all.l_hhm2_h.ph <- brm(bf(leaves ~ (rel.ht  + leaf_habit + myc_group)^2 + 
+                              (1 + rel.ht|gr(phylo, cov = A)) + (1 + rel.ht|SppName) + (1|Study)), 
+                         data = within.sp, 
+                         data2 = list(A = A),
+                         family = Beta(), 
+                         chains = 4, 
+                         iter = 9000, 
+                         warmup = 3000, 
+                         cores = 4, 
+                         control = list(adapt_delta = 0.99,
+                                        max_treedepth = 12),
+                         backend = "cmdstanr",
+                         file = "fits/m.all.l_hhm2_h.ph",
+                         seed = 123)
 
 
 
 
 
-
-
-
-
-m.ac.all.rsl_hhm2_h.ph <- brm(bf(RSL ~ (rel.ht2  + leaf_habit + myc_group)^2 + 
-                                   (1 + rel.ht2|gr(phylo, cov = A)) + (1 + rel.ht2|SppName) + (1|Study)), 
-                              data = across.sp, 
-                              data2 = list(A = A),
-                              family = dirichlet(), 
-                              chains = 4, 
-                              iter = 9000, 
-                              warmup = 3000, 
-                              cores = 4, 
-                              control = list(adapt_delta = 0.99,
-                                             max_treedepth = 12),
-                              backend = "cmdstanr",
-                              file = "fits/m.ac.all.rsl_hhm2_h.ph",
-                              seed = 123)
-
-within.sp %>%
-  data_grid(rel.ht2 = seq_range(rel.ht, n = 51),
-            leaf_habit = c("evergreen", 
-                           "deciduous"),
+root = within.sp %>%
+  data_grid(rel.ht = seq_range(rel.ht, n = 51),
+            leaf_habit = c("deciduous", 
+                           "evergreen"),
             myc_group = c("ECM","AM")) %>%
-  add_epred_draws(m.ac.all.rsl_hhm2_h.ph, re_formula = NA) %>%
-  ggplot(aes(x = rel.ht2, y = .epred, color = .category,
+  add_epred_draws(m.all.r_hhm2_h.ph, re_formula = NA) %>% 
+  add_column(.category = "roots")
+
+stem = within.sp %>%
+  data_grid(rel.ht = seq_range(rel.ht, n = 51),
+            leaf_habit = c("deciduous", 
+                           "evergreen"),
+            myc_group = c("ECM","AM")) %>%
+  add_epred_draws(m.all.s_hhm2_h.ph, re_formula = NA) %>% 
+  add_column(.category = "stems")
+
+leaf = within.sp %>%
+  data_grid(rel.ht = seq_range(rel.ht, n = 51),
+            leaf_habit = c("deciduous", 
+                           "evergreen"),
+            myc_group = c("ECM","AM")) %>%
+  add_epred_draws(m.all.l_hhm2_h.ph, re_formula = NA) %>% 
+  add_column(.category = "leaves")
+
+rsl = rbind(root,stem,leaf)
+rsl$.category = factor(rsl$.category, levels = c("roots","stems","leaves"))
+
+
+figS1 = rsl %>%
+  ggplot(aes(x = rel.ht, y = .epred, color = .category,
              fill = .category)) +
   geom_vline(xintercept = 0, linetype = "dashed", color = "grey") +
   stat_lineribbon(aes(y = .epred), .width = c(0.9),
                   point_interval = "median_qi",
                   linewidth = .2)+
-  #scale_x_continuous(breaks = c(-3,-1,0,1,3), 
-  #                   labels = c("1/8","1/2",1,2,8)) +
+  scale_x_continuous(breaks = c(-3,-1,0,1,3), 
+                     labels = c("1/8","1/2",1,2,8)) +
+  facet_grid(myc_group~leaf_habit) + 
+  scale_color_manual(values = c("#efd08e","#8d8067","#9dbc9b")) +
+  scale_fill_manual(values = c("#efd08e80","#8d806780","#9dbc9b80")) +
+  ylab("Biomass proportion") +
+  xlab("multiplicative change in relative height") +
+  theme_bw() +
+  theme(legend.title = element_blank())
+
+
+ggsave("figS1.png", figS1, units = "mm",
+       width = 180,
+       height = 140,
+       scale = 1.4)
+
+
+
+
+
+##### same model as in Fig4 but within species with at least 10 individuals ####
+
+m.most.rsl_hhm2_h.ph <- brm(bf(RSL ~ (rel.ht  + leaf_habit + myc_group)^2 + 
+                                 (1 + rel.ht|gr(phylo, cov = A)) + (1 + rel.ht|SppName) + (1|Study)), 
+                            data = within.sp %>% 
+                              filter(N>9), 
+                            data2 = list(A = A),
+                            family = dirichlet(), 
+                            chains = 4, 
+                            iter = 9000, 
+                            warmup = 3000, 
+                            cores = 4, 
+                            control = list(adapt_delta = 0.99,
+                                           max_treedepth = 12),
+                            backend = "cmdstanr",
+                            file = "fits/m.most.rsl_hhm2_h.ph",
+                            seed = 123)
+
+summary(m.most.rsl_hhm2_h.ph)
+
+within.sp %>%
+  data_grid(rel.ht = seq_range(rel.ht, n = 51),
+            leaf_habit = c("evergreen", 
+                           "deciduous"),
+            myc_group = c("ECM","AM")) %>%
+  add_epred_draws(m.most.rsl_hhm2_h.ph, re_formula = NA) %>%
+  ggplot(aes(x = rel.ht, y = .epred, color = .category,
+             fill = .category)) +
+  geom_vline(xintercept = 0, linetype = "dashed", color = "grey") +
+  stat_lineribbon(aes(y = .epred), .width = c(0.9),
+                  point_interval = "median_qi",
+                  linewidth = .2)+
+  scale_x_continuous(breaks = c(-3,-1,0,1,3), 
+                     labels = c("1/8","1/2",1,2,8)) +
   facet_grid(myc_group~leaf_habit) + 
   scale_color_manual(values = c("#efd08e","#8d8067","#9dbc9b")) +
   scale_fill_manual(values = c("#efd08e80","#8d806780","#9dbc9b80")) +
